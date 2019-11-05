d825bb2536fe4c1f289f7271525e76f94fca7384
Jesse Whitham <jesse@ripglobal.com>
2019-11-05 18:10:25 +1300

    Add Revision details to Codepipeline message
    * Format slack message further

--- aws-codepipeline-notifier/codepipeline-message-builder.go
+++ aws-codepipeline-notifier/codepipeline-message-builder.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/whithajess/aws-codepipeline-slack-notifications/internal/shared/message"
 	"os"
 
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/service/codepipeline"
 	"github.com/nlopes/slack"
 )
 
@@ -13,18 +16,28 @@ import (
 // # https://docs.aws.amazon.com/codebuild/latest/APIReference/API_BuildPhase.html
 
 func BuildAndSendSlackMessage(detail events.CodePipelineEventDetail, buildID string) {
-	fmt.Println("## BuildSlackMessage:")
-	var blocks []slack.Block // Full Set of blocks that make up the slack message.
+	fmt.Println("## BuildAndSendSlackMessage:")
+	var blocks []slack.Block // Full Set of blocks that make up message to be send to slack.
 
-	// TODO: Top Message
+	pipelineTitleText := fmt.Sprintf("*%s*", detail.Pipeline)
+	pipelineTitleTextBlock := slack.NewTextBlockObject("mrkdwn", pipelineTitleText, false, false)
+
+	blocks = append(blocks, slack.NewSectionBlock(pipelineTitleTextBlock, nil, nil, slack.SectionBlockOptionBlockID("pipeline_title_text_block")))
+
+	var revisionTextBlocks []*slack.TextBlockObject
+	// TODO: Need to not look this up every time
+	revisionSummaryTextBlock := RevisionTextBlockBuilder(&revisionTextBlocks, buildID, detail.Pipeline)
+
+	blocks = append(blocks, slack.NewSectionBlock(revisionSummaryTextBlock, revisionTextBlocks, nil, slack.SectionBlockOptionBlockID("revision_details")))
+
+	statusTitleText := slack.NewTextBlockObject("mrkdwn", "*Pipeline Status:*", false, false)
+
+	blocks = append(blocks, slack.NewSectionBlock(statusTitleText, nil, nil, slack.SectionBlockOptionBlockID("status_title_text")))
 
 	var detailTextBlocks []*slack.TextBlockObject
-
 	DetailTextBlockBuilder(&detailTextBlocks, detail)
 
-	textInfo := slack.NewTextBlockObject("mrkdwn", "*Pipeline Status:*", false, false)
-
-	blocks = append(blocks, slack.NewSectionBlock(textInfo, detailTextBlocks, nil, slack.SectionBlockOptionBlockID("first_phase_block")))
+	blocks = append(blocks, slack.NewSectionBlock(nil, detailTextBlocks, nil, slack.SectionBlockOptionBlockID("status_details")))
 
 	channelID := os.Getenv("CodePipelineSlackChannelID")
 
@@ -33,7 +46,45 @@ func BuildAndSendSlackMessage(detail events.CodePipelineEventDetail, buildID str
 	// TODO: Log links etc.
 }
 
-func DetailTextBlockBuilder(phaseTextBlocks *[]*slack.TextBlockObject, detail events.CodePipelineEventDetail ) {
+func RevisionTextBlockBuilder(revisionTextBlocks *[]*slack.TextBlockObject, buildID string, pipelineName string) *slack.TextBlockObject{
+	fmt.Println("Message Lookup")
+	// Create the session that the CodePipeline service will use.
+	awsSession := session.Must(session.NewSession())
+
+	// Create the CodePipeline service client to make the query request with.
+	svc := codepipeline.New(awsSession)
+
+	// https://docs.aws.amazon.com/sdk-for-go/api/service/codepipeline/#CodePipeline.GetPipelineExecutionRequest
+	req, resp := svc.GetPipelineExecutionRequest(
+		&codepipeline.GetPipelineExecutionInput{
+			PipelineExecutionId: aws.String(buildID),
+			PipelineName:        aws.String(pipelineName),
+		})
+
+	err := req.Send()
+	// TODO: Clean this up
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	revisionSummaryTextBlock := slack.NewTextBlockObject("mrkdwn", "",false, false)
+	revisionURL := ""
+
+	// TODO: Clean this up
+	if len(resp.PipelineExecution.ArtifactRevisions) > 0 {
+		revisionInfo := resp.PipelineExecution.ArtifactRevisions[0]
+		revisionSummaryTextBlock.Text = *revisionInfo.RevisionSummary
+		revisionURL = fmt.Sprintf("<%s|Commit: %.7s>", *revisionInfo.RevisionUrl, *revisionInfo.RevisionId)
+	}
+
+	// TODO: Clean this up
+	revisionTextBlock2 := slack.NewTextBlockObject("mrkdwn", revisionURL,false, false)
+	*revisionTextBlocks = append(*revisionTextBlocks, revisionTextBlock2)
+
+	return revisionSummaryTextBlock
+}
+
+func DetailTextBlockBuilder(detailTextBlocks *[]*slack.TextBlockObject, detail events.CodePipelineEventDetail ) {
 	statusIconMapping := map[string]string{
 		"": 									   message.BuildPhasesUnknown,
 		string(events.CodePipelineStateStarted):   message.BuildPhasesInProgress,
@@ -44,11 +95,11 @@ func DetailTextBlockBuilder(phaseTextBlocks *[]*slack.TextBlockObject, detail ev
 		events.CodePipelineStateSuperseded:        message.BuildPhasesStopped,
 	}
 
-	phaseTextBlock := slack.NewTextBlockObject(
+	detailTextBlock := slack.NewTextBlockObject(
 			"plain_text",
 		    fmt.Sprintf("%s %s", string(statusIconMapping[string(detail.State)]), string(detail.Stage)),
 			true,
 			false,
 	)
-	*phaseTextBlocks = append(*phaseTextBlocks, phaseTextBlock)
+	*detailTextBlocks = append(*detailTextBlocks, detailTextBlock)
 }
